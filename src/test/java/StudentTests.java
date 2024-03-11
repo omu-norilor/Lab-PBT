1cd9268c229ae4177dd70faeebe8e7529a82bb4e
Rares Descas <91518641+omu-norilor@users.noreply.github.com>
2024-03-11 15:37:57 +0200

    inital commit

---
+++ src/test/java/StudentTests.java
@@ -0,0 +1,32 @@
+import domain.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+public class StudentTests {
+
+    @Test
+    public void testAddStudent(){
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        Student student = new Student("1", "Mihai", 1, "Mihai@mihai.mihai");
+        Student student1 = service.addStudent(student);
+        Assertions.assertEquals(student, student1);
+//        Assertions.assertNull(student1);
+    }
+}
